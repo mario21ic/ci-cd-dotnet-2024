9f06e9f5adc9b0fcee38c83e22a1343802385863
Mario IC <mario21ic@gmail.com>
2024-06-05 01:38:31 -0500

    feat(global) updating scripts

---
+++ scripts/docker_unit_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+BUILD=$1
+echo "Running unit tests in Docker: "$BUILD
+
+docker run --rm -u 0:0 -v $PWD/:/src/ -w /src/ mcr.microsoft.com/dotnet/sdk:6.0 ./scripts/unit_tests.sh $BUILD
--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -4,4 +4,4 @@
 BUILD=$1
 echo "Running unit tests: "$BUILD
 
-docker run --rm -u 0:0 -v $PWD/MyApp.tests/:/src/ -w /src/ mcr.microsoft.com/dotnet/sdk:6.0 dotnet test
+cd MyApp.tests/ && dotnet test
