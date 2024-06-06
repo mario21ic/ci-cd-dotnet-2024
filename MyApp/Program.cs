c0aeeae9f5cdf1f0c1557d1f638b0826410d07cc
Mario IC <mario21ic@gmail.com>
2024-06-06 01:49:26 -0500

    feat(global) adding sonarqube integration

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+DateTime now = DateTime.Now;
+
+// Determina el saludo basado en la hora del día
+if (now.Hour < 12)
+{
+    Console.WriteLine("Buenos días");
+}
+else if (now.Hour >= 12 && now.Hour < 18)
+{
+    Console.WriteLine("Buenas tardes");
+}
+else
+{
+    Console.WriteLine("Buenas noches");
+}
