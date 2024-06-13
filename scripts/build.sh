dd024175d0cc36720f971d26ee83baa0bf6fb053
Mario IC <mario21ic@gmail.com>
2024-06-13 18:41:30 -0500

    feat(global) fixeando el build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ set -xe
 BUILD=$1
 echo  "BUILD: "$BUILD
 
-docker run --rm -u 0:0 -v $PWD/MyApp/:/src/ -w /src/ mcr.microsoft.com/dotnet/sdk:6.0 dotnet runrun
+docker run --rm -u 0:0 -v $PWD/MyApp/:/src/ -w /src/ mcr.microsoft.com/dotnet/sdk:6.0 dotnet run
