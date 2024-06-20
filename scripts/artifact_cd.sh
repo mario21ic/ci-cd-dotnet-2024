d3c99b515fd532d0e35864a0105fd8697cb0b2e2
Mario IC <mario21ic@gmail.com>
2024-06-20 12:12:14 -0500

    feat(deploy) updating

--- scripts/artifact_cd.sh
+++ scripts/artifact_cd.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# syntax: ./scripts/artifact_cd.sh mario21ic/dotnet-api:v2 123.zip
 set -xe
 
 export DOCKER_IMAGE=$1
+FILENAME=$2
+
 envsubst '${DOCKER_IMAGE}' < stack.yaml.template > stack.yaml
 #sed -i "s|latest|${DOCKER_IMAGE}|g" stack.yaml
 
-rm -rf artifact.zip
-zip -r artifact.zip scripts/ appspec.yml stack.yaml
+rm -rf $FILENAME
+zip -r $FILENAME scripts/ appspec.yml stack.yaml
