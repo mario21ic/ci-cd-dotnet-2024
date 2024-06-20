f333e3c585f56b04eaf7061fe12b16bffe493ac7
Mario IC <mario21ic@gmail.com>
2024-06-20 02:27:24 -0500

    feat(deploy) updating delivery

--- scripts/artifact_cd.sh
+++ scripts/artifact_cd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -xe
 
+export DOCKER_IMAGE=$1
+envsubst '${DOCKER_IMAGE}' < stack.yaml.template > stack.yaml
+#sed -i "s|latest|${DOCKER_IMAGE}|g" stack.yaml
+
 rm -rf artifact.zip
 zip -r artifact.zip scripts/ appspec.yml stack.yaml
