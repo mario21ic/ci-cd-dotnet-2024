305b01e60d47b50f3da9c8ca611b986a20b4237a
Mario IC <mario21ic@gmail.com>
2024-06-20 11:32:10 -0500

    feat(deploy) adding hostname

--- MyApi/WeatherForecast.cs
+++ MyApi/WeatherForecast.cs
@@ -1,5 +1,7 @@
 namespace MyApi;
 
+using System.Net;
+
 public class WeatherForecast
 {
     public DateTime Date { get; set; }
@@ -9,4 +11,6 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+
+    public string HostName => Dns.GetHostName();
 }
